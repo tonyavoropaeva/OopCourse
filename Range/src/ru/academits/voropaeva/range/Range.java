343b358fb8848f4c58f1f0e4a0276da15b3d6335
tonyavoropaeva <voropaevatonya@gmai.com>
2022-07-15 10:43:34 +0700

    Выполнила задачу Range

--- Range/src/ru/academits/voropaeva/range/Range.java
+++ Range/src/ru/academits/voropaeva/range/Range.java
@@ -1,7 +1,35 @@
 package ru.academits.voropaeva.range;
 
 public class Range {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && to >= number;
     }
 }
--- Range/src/ru/academits/voropaeva/range_main/Main.java
+++ Range/src/ru/academits/voropaeva/range_main/Main.java
@@ -1,4 +1,23 @@
 package ru.academits.voropaeva.range_main;
 
+import ru.academits.voropaeva.range.Range;
+
 public class Main {
+    public static void main(String[] args) {
+        Range segmentAB = new Range(-10, 7);
+
+        System.out.printf("Отрезок на числовом диапазоне равен от a = %.2f до b = %.2f %n", segmentAB.getFrom(), segmentAB.getTo());
+
+        segmentAB.setFrom(-16);
+        segmentAB.setTo(20);
+
+        double a = segmentAB.getFrom();
+        double b = segmentAB.getTo();
+
+        System.out.printf("Новый отрезок равен a = %.2f до b = %.2f %n", a, b);
+        System.out.printf("Длина ab = %.2f %n", segmentAB.getLength());
+
+        double point1 = 10;
+        System.out.printf("Лежит ли точка %.2f с на отрезке ab? - Ответ: %s", point1, segmentAB.isInside(10));
+    }
 }
