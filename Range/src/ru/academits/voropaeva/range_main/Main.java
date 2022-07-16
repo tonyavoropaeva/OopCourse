6d257cff4aa8dc07e8a409a72afd9dfc4013942a
tonyavoropaeva <voropaevatonya@gmai.com>
2022-07-16 10:12:56 +0700

    Изменение JDK и кодировки

--- Range/src/ru/academits/voropaeva/range_main/Main.java
+++ Range/src/ru/academits/voropaeva/range_main/Main.java
@@ -4,20 +4,20 @@ import ru.academits.voropaeva.range.Range;
 
 public class Main {
     public static void main(String[] args) {
-        Range segmentAB = new Range(-10, 7);
+        Range rangeAB = new Range(-10, 7);
 
-        System.out.printf("Отрезок на числовом диапазоне равен от a = %.2f до b = %.2f %n", segmentAB.getFrom(), segmentAB.getTo());
+        System.out.printf("Отрезок на числовом диапазоне равен от a = %.2f до b = %.2f %n", rangeAB.getFrom(), rangeAB.getTo());
 
-        segmentAB.setFrom(-16);
-        segmentAB.setTo(20);
+        rangeAB.setFrom(-16);
+        rangeAB.setTo(20);
 
-        double a = segmentAB.getFrom();
-        double b = segmentAB.getTo();
+        double a = rangeAB.getFrom();
+        double b = rangeAB.getTo();
 
         System.out.printf("Новый отрезок равен a = %.2f до b = %.2f %n", a, b);
-        System.out.printf("Длина ab = %.2f %n", segmentAB.getLength());
+        System.out.printf("Длина ab = %.2f %n", rangeAB.getLength());
 
         double point1 = 10;
-        System.out.printf("Лежит ли точка %.2f с на отрезке ab? - Ответ: %s", point1, segmentAB.isInside(10));
+        System.out.printf("Лежит ли точка %.2f с на отрезке ab? - Ответ: %s", point1, rangeAB.isInside(10));
     }
 }
