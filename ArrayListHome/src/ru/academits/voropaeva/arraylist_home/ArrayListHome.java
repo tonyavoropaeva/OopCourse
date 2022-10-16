5d68eadf6ba15dd161453a389c93acd931a8081a
tonyavoropaeva <voropaevatonya@gmai.com>
2022-10-17 00:37:45 +0700

    Исправила задачу ArrayListHome

--- ArrayListHome/src/ru/academits/voropaeva/arraylist_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/voropaeva/arraylist_home/ArrayListHome.java
@@ -1,59 +1,53 @@
 package ru.academits.voropaeva.arraylist_home;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         // Прочитать в список все строки из файла
 
         ArrayList<String> lines = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(new FileInputStream("text.txt"))) {
-            while (scanner.hasNextLine()) {
-                lines.add(scanner.nextLine());
+        try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"))) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка, файл не найден");
         }
 
-        System.out.println(lines);
+        System.out.println("Данные из файла: " + lines);
 
         /* Есть список из целых чисел. Удалить из него все четные числа. В
            этой задаче новый список создавать нельзя */
 
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 10, 2, 5, 6, 0, 12, 7, 9, 3, 9, 10, 17, 17, 2, 3, 17, 21));
 
-        if (numbers.get(2) == 2) {
-            numbers.remove(numbers.get(2));
-        }
+        System.out.println("Список: " + numbers);
 
-        for (int i = 0; i < numbers.size(); ++i) {
-            if (numbers.get(i) % 2 == 0) {
-                numbers.remove(numbers.get(i));
-            }
-        }
+        numbers.removeIf(number -> number % 2 == 0);
 
-        // или numbers.removeIf(number -> number % 2 == 0);
-
-        System.out.println(numbers);
+        System.out.println("Список после удаления четных чисел: " + numbers);
 
         /* Есть список из целых чисел, в нём некоторые числа могут
            повторяться. Надо создать новый список, в котором будут
            элементы первого списка в таком же порядке, но без
            повторений */
 
-        ArrayList<Integer> numbersNoRepeats = new ArrayList<>();
+        ArrayList<Integer> numbersNoRepeats = new ArrayList<>(numbers.size());
 
         for (Integer number : numbers) {
-            boolean isThere = numbersNoRepeats.contains(number);
 
-            if (!isThere) {
+            if (!numbersNoRepeats.contains(number)) {
                 numbersNoRepeats.add(number);
             }
         }
 
-        System.out.println(numbersNoRepeats);
+        System.out.println("Новый список после удаления повторяющихся чисел: " + numbersNoRepeats);
     }
 }
