9c7d08c4973361edb963d5ca4d27b41070b6e0f7
tonyavoropaeva <voropaevatonya@gmai.com>
2023-04-10 18:46:17 +0700

    Исправила csv_parser

--- CSVParser/src/ru/academits/voropaeva/csv_parser/Main.java
+++ CSVParser/src/ru/academits/voropaeva/csv_parser/Main.java
@@ -4,24 +4,35 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Передано неправильное количество аргументов, сейчас их: " + args.length);
+
+            return;
+        }
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]));
              PrintWriter printWriter = new PrintWriter(args[1])
         ) {
             printWriter.println("<!DOCTYPE HTML>");
-            printWriter.println("<head>");
-            printWriter.println("\t<meta charset=\"utf-8\">");
-            printWriter.println("\t<title>Таблица</title>");
-            printWriter.println("</head>");
-            printWriter.println("<body>");
-            printWriter.println("\t<table>");
+            printWriter.println("<html>");
+            printWriter.println("\t<head>");
+            printWriter.println("\t\t<meta charset=\"utf-8\">");
+            printWriter.println("\t\t<title>Таблица</title>");
+            printWriter.println("\t</head>");
+            printWriter.println("\t<body>");
+            printWriter.println("\t\t<table>");
 
-            boolean isQuoteOpen = false; //если кавычки открыты
+            boolean isQuoteOpen = false;
             String line;
 
             while ((line = bufferedReader.readLine()) != null) {
-                if (!isQuoteOpen) { //если мы не в кавычках
-                    printWriter.println("\t\t<tr>");
-                    printWriter.print("\t\t\t<td>");
+                if (line.length() == 0) {
+                    continue;
+                }
+
+                if (!isQuoteOpen) {
+                    printWriter.println("\t\t\t<tr>");
+                    printWriter.print("\t\t\t\t<td>");
                 }
 
                 for (int i = 0; i < line.length(); ++i) {
@@ -38,38 +49,38 @@ public class Main {
                     }
 
                     if (currentChar == '&') {
-                        printWriter.print("&amp");
+                        printWriter.print("&amp;");
                         continue;
                     }
 
-                    if (isQuoteOpen) { //если в кавычках
-                        if (currentChar == '\"') { //если попалась кавычка
-                            if (i == line.length() - 1) { //и мы в конце строки -> выходим из кавычек
+                    if (isQuoteOpen) {
+                        if (currentChar == '\"') {
+                            if (i == line.length() - 1) {
                                 isQuoteOpen = false;
-                            } else if (line.charAt(i + 1) == '\"') { //если дальше кавычка
-                                printWriter.print("\""); //печатаем одну кавычку
+                            } else if (line.charAt(i + 1) == '\"') {
+                                printWriter.print("\"");
 
-                                i += 1;
+                                ++i;
                             } else {
-                                isQuoteOpen = false; // закрываем кавычку
+                                isQuoteOpen = false;
                             }
-                        } else if (i == line.length() - 1) { //если мы вконце строки и в кавычках, то есть перевод строки
+                        } else if (i == line.length() - 1) {
                             printWriter.print(currentChar);
                             printWriter.print("<br/>");
                         } else {
                             printWriter.print(currentChar);
                         }
-                    } else { //вне кавычек
+                    } else {
                         if (currentChar == ',') {
                             printWriter.println("</td>");
 
                             if (i == line.length() - 1) {
-                                printWriter.print("\t\t\t<td>");
+                                printWriter.print("\t\t\t\t<td>");
                             } else {
-                                printWriter.print("\t\t\t<td>");
+                                printWriter.print("\t\t\t\t<td>");
                             }
-                        } else if (currentChar == '"') { //если попалась кавычка
-                            isQuoteOpen = true; // открываем кавычки
+                        } else if (currentChar == '"') {
+                            isQuoteOpen = true;
                         } else {
                             printWriter.print(currentChar);
                         }
@@ -78,13 +89,14 @@ public class Main {
 
                 if (!isQuoteOpen) {
                     printWriter.println("</td>");
-                    printWriter.print("\t\t");
+                    printWriter.print("\t\t\t");
                     printWriter.println("</tr>");
                 }
             }
 
-            printWriter.println("\t</table>");
-            printWriter.println("</body>");
+            printWriter.println("\t\t</table>");
+            printWriter.println("\t</body>");
+            printWriter.println("</html>");
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден: " + e.getMessage());
         } catch (IOException e) {
--- Temperature/src/ru/academits/voropaeva/temperature/main/Main.java
+++ Temperature/src/ru/academits/voropaeva/temperature/main/Main.java
@@ -5,14 +5,16 @@ import ru.academits.voropaeva.temperature.view.SwingView;
 import ru.academits.voropaeva.temperature.view.View;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Model model = new Converter(Arrays.asList(
+        List<Scale> scales = Arrays.asList(
                 new CelsiusScale(),
                 new KelvinScale(),
-                new FahrenheitScale()));
+                new FahrenheitScale());
 
+        Model model = new Converter(scales);
         View view = new SwingView(model);
 
         view.initialize();
